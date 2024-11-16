39c9486157ac3b572adb68fe387aa55f8f9ba022
bagus-net <you@example.com>
2024-11-16 22:30:45 +0700

    details  product done

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Brand;
 use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
@@ -19,4 +20,22 @@ class FrontController extends Controller
         session()->put('category_id',$category->id);
         return view('front.brands',compact('category'));
 }
+
+    public function brand(Brand $brand){
+        $category_id = session()->get('category_id');
+        $category = Category::find($category_id);
+        $products = Product::where('brand_id', $brand->id)
+            ->where('category_id', $category_id)
+            ->latest()
+            ->get();
+
+        return view('front.gadgets', compact('brand', 'products','category'));
+    }
+
+    public function details(Product $product){
+
+        return view('front.details',compact('product'));
+
+    }
+
 }
--- resources/views/front/brands.blade.php
+++ resources/views/front/brands.blade.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="{{asset('output.css')}}" rel="stylesheet" />
-		<link href="{{asset('main.css')}}" rel="stylesheet" />
+	<link href="{{asset('main.css')}}" rel="stylesheet" />
 	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap"
 		rel="stylesheet" />
 </head>
@@ -13,19 +13,19 @@
 <body>
 	<main class="max-w-[640px] mx-auto min-h-screen flex flex-col relative has-[#Bottom-nav]:pb-[144px]">
 		<div id="Top-navbar" class="flex items-center justify-between px-5 pt-5 w-full">
-			<a href="index.html">
+			<a href="{{route('front.index')}}">
 				<div class="size-[44px] flex shrink-0">
-					<img src="assets/images/icons/arrow-left.svg" alt="icon" />
+					<img src="{{asset('assets/images/icons/arrow-left.svg')}}" alt="icon" />
 				</div>
 			</a>
 			<p class="text-lg leading-[27px] font-semibold">Choose Brand</p>
 			<button class="size-[44px] flex shrink-0">
-				<img src="assets/images/icons/more.svg" alt="icon" />
+				<img src="{{asset('assets/images/icons/more.svg')}}" alt="icon" />
 			</button>
 		</div>
 		<a id="promo" href="#" class="px-5 mt-[30px]">
 			<div class="w-full aspect-[353/100] flex shrink-0 overflow-hidden rounded-2xl">
-				<img src="assets/images/backgrounds/promo.png" class="w-full h-full object-cover" alt="promo" />
+				<img src="{{asset('assets/images/backgrounds/promo.png')}}" class="w-full h-full object-cover" alt="promo" />
 			</div>
 		</a>
 		<section id="Brand" class="flex flex-col gap-[30px] mt-[30px] px-5">
